5f8a85f089acc384c409097204dd036f69f81b0d
Hancheol Cho <chcbaram@gmail.com>
2025-10-09 22:00:14 +0900

    CubeMX 설정 변경

--- firmware/convex-cube/Core/Src/tim.c
+++ firmware/convex-cube/Core/Src/tim.c
@@ -172,8 +172,8 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     hdma_tim3_ch1.Init.Direction = DMA_MEMORY_TO_PERIPH;
     hdma_tim3_ch1.Init.PeriphInc = DMA_PINC_DISABLE;
     hdma_tim3_ch1.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_tim3_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_tim3_ch1.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_tim3_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_tim3_ch1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
     hdma_tim3_ch1.Init.Mode = DMA_NORMAL;
     hdma_tim3_ch1.Init.Priority = DMA_PRIORITY_LOW;
     hdma_tim3_ch1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
